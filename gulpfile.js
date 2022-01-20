15745c3d4df9d6593b97aba30695011d56c357f2
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-01-20 16:40:28 +0700

    add comment config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest }  = require("gulp")
 const minify = require("gulp-minify")
 
 const minifyjs = () => src('src/diff.js', { allowEmpty: true }) 
-        .pipe(minify({noSource: true}))
+        .pipe(minify({noSource: true, preserveComments: "some"}))
         .pipe(dest('dest/'))
 
 exports.default = minifyjs
